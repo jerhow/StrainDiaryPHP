7c4173d778e19654a1942b4244105524048e3425
Jerry Howard <jerryhoward@fastmail.fm>
2021-12-31 14:27:43 -0500

    Get routing installed and working

--- public/index.php
+++ public/index.php
@@ -1,2 +1,29 @@
 <?php
-phpinfo();
+// phpinfo(); exit();
+
+// Require composer autoloader
+require '/app/vendor/autoload.php';
+
+// Create Router instance
+$router = new \Bramus\Router\Router();
+
+// $router->setBasePath('/');
+
+// Define routes
+// ...
+
+$router->get('foo', function() {
+    echo 'Foo Page Contents';
+});
+
+$router->get('/', function() {
+    echo 'Home Page Contents';
+});
+
+// This route handling function will only be executed when visiting http(s)://www.example.org/about
+$router->get('/about', function() {
+    echo 'About Page Contents';
+});
+
+// Run it!
+$router->run();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bbd84f5647fe2198b3c10adb7981cff12ab38655',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bbd84f5647fe2198b3c10adb7981cff12ab38655',
+            'dev_requirement' => false,
+        ),
+        'bramus/router' => array(
+            'pretty_version' => '1.6.1',
+            'version' => '1.6.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../bramus/router',
+            'aliases' => array(),
+            'reference' => '55657b76da8a0a509250fb55b9dd24e1aa237eba',
+            'dev_requirement' => false,
+        ),
+    ),
+);
