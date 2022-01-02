ba2006d7f5b5e44bce8d79ee902b8973a99a59a5
Jerry Howard <jerryhoward@fastmail.fm>
2022-01-01 23:05:08 -0500

    Login form and session instantiation

--- public/controllers.php
+++ public/controllers.php
@@ -3,16 +3,57 @@
 namespace Controllers;
 
 function home() {
+    
+    front_gate_check();
+    
+    echo 'Home Page Contents';
+
+    echo "<br /><br />Session data:<br /><br />";
+    echo "uid: " . $_SESSION['uid'];
+
+    // echo var_export($_SESSION, false);
+}
+
+/**
+ * Either returns true, or redirects the user and halts execution
+ */
+function front_gate_check() {
     // Check whether they've answered the age question at the front gate
     if(isset($_COOKIE['passed_front_gate']) && $_COOKIE['passed_front_gate'] === 'Yes') {
-        // User can stay here
+        return true;
     } else {
         // Send the user to the front gate
         header("Location: /front-gate");
         die();
     }
-    
-    echo 'Home Page Contents';
+}
+
+function login_GET($msg = '') {
+
+    require_once('templates/header.php');
+    require_once('templates/login.php');
+    require_once('templates/footer.php');
+}
+
+function login_POST() {
+
+    $email = $_POST['email'];
+    $pwd = $_POST['pwd'];
+
+    if($email === 'abc' && $pwd === 'def') { // testing obvs...
+        $_SESSION['uid'] = 'u1234';
+        $_SESSION['start'] = time(); // Taking now logged in time.
+        
+        // End session 30 minutes from the starting time.
+        $_SESSION['expire'] = $_SESSION['start'] + (30 * 60);
+
+        header("Location: /");
+        die();
+    } else {
+        login_GET('Incorrect email or password');
+    }
+
+    // we should never get here
 }
 
 function front_gate_GET() {
--- public/index.php
+++ public/index.php
@@ -8,11 +8,18 @@ main();
 
 function main() {
 
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
     $router = new \Bramus\Router\Router();
 
     $router->get('/', '\Controllers\home');
-    $router->get('front-gate', '\Controllers\front_gate_GET');
-    $router->post('front-gate', '\Controllers\front_gate_POST');
+    $router->get('/front-gate', '\Controllers\front_gate_GET');
+    $router->post('/front-gate', '\Controllers\front_gate_POST');
+
+    $router->get('/login', '\Controllers\login_GET');
+    $router->post('/login', '\Controllers\login_POST');
 
     $router->run();
 
